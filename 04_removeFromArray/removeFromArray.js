74ea81249695d4500312dc76c856a256ab4f9727
nschubi <dev@schulzebilk.de>
2024-04-06 10:23:38 +0200

    solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let filtered = arr.filter((value) => {
+        return !args.includes(value)
+    });
+    return filtered;
 };
 
 // Do not edit below this line
