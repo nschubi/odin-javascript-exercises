e72c03e5e167f62b2c3a0b8f03886f6056da339c
nschubi <dev@schulzebilk.de>
2024-04-06 10:12:26 +0200

    solve 02_repeatStrings

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times=0) {
+    let result = "";
+    if(times < 0){
+        return "ERROR";
+    }
+    while(times > 0){
+        result += word;
+        times--;
+    }
+    return result;
 };
 
 // Do not edit below this line
