9bd779c33ca6e53f9e3a4258cb4358545f0586a2
nschubi <dev@schulzebilk.de>
2024-04-06 10:33:13 +0200

    solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(typeof a !== "number" || typeof b !== "number"){
+        return "ERROR";
+    }
+    if(a < 0 || b < 0){
+        return "ERROR";
+    }
+    let gr = a >= b ? a : b;
+    let kl = a < b ? a : b;
+    let sum = 0;
+    for(let i = kl; i <= gr; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
