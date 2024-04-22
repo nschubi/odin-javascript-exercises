5e1d974573a88595ffb43295a1611e50eabddd39
nschubi <dev@schulzebilk.de>
2024-04-22 13:09:46 +0200

    solve 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(
+    (ergebnis,number) =>  ergebnis + number
+    , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(
+    (ergebnis,number) =>  ergebnis * number);
 };
 
-const power = function() {
-	
+const power = function(a,p) {
+	let pow = 1;
+  for(let i = 0; i < p; i++){
+    pow = pow * a;
+  }
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fac = 1;
+  for(let i = a; i > 0; i--){
+    fac = fac * i;
+  }
+  return fac;
 };
 
 // Do not edit below this line
