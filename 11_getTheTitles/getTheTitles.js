65ba61de337678e24974bd563674a0e45f2d40aa
nschubi <dev@schulzebilk.de>
2024-04-22 13:42:21 +0200

    solve 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = Array.from(books).map(book => book.title);
+    return titles;
 };
 
 // Do not edit below this line
