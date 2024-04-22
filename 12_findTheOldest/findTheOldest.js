0c7dd0808321c5eab4ab551b3fc55b19af8fd48c
nschubi <dev@schulzebilk.de>
2024-04-22 13:51:10 +0200

    solve 12_findOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const person = people.reduce( (obj, p) => {
+        let age = 'yearOfDeath' in p ? p.yearOfDeath - p.yearOfBirth : new Date().getFullYear() - p.yearOfBirth;
+        if(age > obj.age){
+            obj.name = p.name;
+            obj.age = age;
+        }
+        return obj;
+    }, {
+        name: "",
+        age: 0
+    });
+    return person;
 };
 
 // Do not edit below this line
