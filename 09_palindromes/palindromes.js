0043f285d4c15ecf35773602bdb81f8ed291b29e
nschubi <dev@schulzebilk.de>
2024-04-22 13:29:12 +0200

    solve 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let split = word.toLowerCase().split('').filter(word => !['!','.','?',' ',','].includes(word));
+    let cleanWord = split.join('');
+    let reverse = split.reverse();
+    let reverseWord = reverse.join('');
+    return cleanWord === reverseWord;
 };
 
 // Do not edit below this line
